41ebf23c035bda484ee6694ba9b78737640f62f4
Abbas Al Khalifa <info@devf5r.com>
2022-10-11 01:19:38 +0300

    555555555555555555555555555555555555555

--- index.js
+++ index.js
@@ -1,60 +1,72 @@
-const dotenv = require("dotenv");
-dotenv.config();
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 const http = require("http").Server(app);
-const port = process.env.PORT || 4000;
-const cors = require("cors");
-
-app.use(cors());
-
 const io = require("socket.io")(http);
 
-//لعقد معلومات المستخدمين
+//To holding users information 
 const socketsStatus = {};
+/* 
+app.configure('development', function() {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+  
+  app.configure('production', function() {
+    app.use(express.errorHandler());
+  });
 
-//التكوين وتعيين المقاود للتعبير
+io.configure(function () { 
+    io.set("transports", ["xhr-polling"]); 
+    io.set("polling duration", 10); 
+  }); */
+
+io.on("connection", function (socket) {
+    const socketId = socket.id;
+    socketsStatus[socket.id] = {};
+  
+  
+    console.log("connect");
+  
+    socket.on("voice", function (data) {
+  
+      var newData = data.split(";");
+      newData[0] = "data:audio/ogg;";
+      newData = newData[0] + newData[1];
+  
+      for (const id in socketsStatus) {
+  
+        if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
+          socket.broadcast.to(id).emit("send", newData);
+      }
+  
+    });
+  
+    socket.on("userInformation", function (data) {
+      socketsStatus[socketId] = data;
+  
+      io.sockets.emit("usersUpdate",socketsStatus);
+    });
+  
+  
+    socket.on("disconnect", function () {
+      delete socketsStatus[socketId];
+    });
+  
+  });
+
+//config and set handlebars to express
 const customHandlebars = handlebars.create({ layoutsDir: "./views" });
 
 app.engine("handlebars", customHandlebars.engine);
 app.set("view engine", "handlebars");
 
-//تمكين وصول المستخدم إلى المجلد العام
-app.use(express.static(__dirname + "/public"));
+//enable user access to public folder 
+app.use("/files", express.static("public"));
 
-app.get("/home", (req, res) => {
-  res.render("index");
+app.get("/" , (req , res)=>{
+    res.render("index");
 });
 
-io.on("connection", function (socket) {
-  const socketId = socket.id;
-  socketsStatus[socket.id] = {};
-
-  console.log("connect");
-
-  socket.on("voice", function (data) {
-    var newData = data.split(";");
-    newData[0] = "data:audio/ogg;";
-    newData = newData[0] + newData[1];
-
-    for (const id in socketsStatus) {
-      if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
-        socket.broadcast.to(id).emit("send", newData);
-    }
-  });
-
-  socket.on("userInformation", function (data) {
-    socketsStatus[socketId] = data;
-
-    io.sockets.emit("usersUpdate", socketsStatus);
-  });
-
-  socket.on("disconnect", function () {
-    delete socketsStatus[socketId];
-  });
-});
-
-http.listen(port, () => {
-  console.log(`Server running at ${port}`);
+http.listen(process.env.PORT || 3000, () => {
+  console.log("the app is run in port 3000!");
 });
