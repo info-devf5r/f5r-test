35d8cf9ea1eb06b80aa7501ec23136be9164b4ae
Abbas Al Khalifa <115319928+info-devf5r@users.noreply.github.com>
2022-10-12 10:14:11 +0300

    z\xc

--- src/SocketHolder.js
+++
@@ -1,9 +0,0 @@
-export default {
-  set socket(socket) {
-    this._socket = socket;
-  },
-
-  get socket() {
-    return this._socket;
-  },
-};
--- src/util.js
+++ src/util.js
@@ -1,56 +1,19 @@
-import { findIndex } from "lodash-es";
-
-const testLocalStorage = () => {
-  const test = "test";
-  try {
-    localStorage.setItem(test, test);
-    localStorage.removeItem(test);
-    return true;
-  } catch (e) {
-    return false;
-  }
+module.exports.pick = (obj, ...attr) => {
+  return attr.reduce((acc, k) => {
+    if (obj.hasOwnProperty(k)) {
+      acc[k] = obj[k];
+    }
+    return acc;
+  }, {});
 };
 
-export const isLocalStorageAvailable = testLocalStorage();
-
-export function formatDuration(duration) {
-  const d = Math.ceil(Math.max(duration, 0));
-  const days = Math.floor(d / 86400);
-  const hours = Math.floor((d - days * 86400) / 3600);
-  const minutes = Math.floor((d - days * 86400 - hours * 3600) / 60);
-  const seconds = Math.ceil(d) - days * 86400 - hours * 3600 - minutes * 60;
-
-  const output = [];
-  if (days > 0) {
-    output.push(days + "d");
-  }
-  if (days > 0 || hours > 0) {
-    output.push(hours + "h");
-  }
-  if (days > 0 || hours > 0 || minutes > 0) {
-    output.push(minutes + "m");
-  }
-  output.push(seconds + "s");
-  return output.join(" ");
-}
-
-/**
- * lodash remove() does not play well with Vue.js
- */
-export function remove(array, predicate) {
-  const index =
-    typeof predicate === "object"
-      ? findIndex(array, predicate)
-      : array.indexOf(predicate);
-  return index === -1 ? [] : array.splice(index, 1);
-}
-
-export function pushUniq(array, elem) {
-  if (!array.includes(elem)) {
-    array.push(elem);
-  }
-}
-
-export function percentage(value, total) {
-  return total === 0 ? 0 : ((value / total) * 100).toFixed(1);
-}
+module.exports.installTimerFunctions = (obj, opts) => {
+  obj.setTimeoutFn = function(fn, time) {
+    return setTimeout(() => {
+      fn()
+    }, time)
+  };
+  obj.clearTimeoutFn = function(timer) {
+    clearTimeout(timer)
+  };
+};
