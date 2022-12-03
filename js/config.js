650507c0b25c482eb7d080188c12d2aa93ea1ef1
Abbas Al Khalifa <115319928+info-devf5r@users.noreply.github.com>
2022-12-03 07:52:19 +0300

    0

--- js/config.js
+++
@@ -1,7 +0,0 @@
-function getWebsiteUrl (){
-	if(location.protocol === 'http'){
-		return "http://localhost:81/youtube-playlist-adi-levi";
-	}else{
-		return "https://youtube-playlist-adi-levi.herokuapp.com";
-	}
-}
